2b85838ea87794dc09a2d8e758005b25ee9689d8
eyadrmh-star <eyadrmh@gmail.com>
2026-07-08 00:57:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3133,8 +3133,7 @@ window.addEventListener('load', function() {
         
         var tagline = loginCard.querySelector('p');
         var personImg = document.createElement('div');
-        personImg.style.cssText = 'height:300px;width:110%;margin-left:-5%;background-image:url(https://i.postimg.cc/nrHrRZnv/Chat-GPT-Image-May-6-2026-02-11-58-PM.png);background-size:cover;background-position:center -5%;border-radius:15px;margin-top:15px;margin-bottom:15px;';
-        
+personImg.style.cssText = 'height:300px;width:110%;margin-left:-5%;background-image:url(https://i.postimg.cc/nrHrRZnv/Chat-GPT-Image-May-6-2026-02-11-58-PM.png);background-size:cover;background-position:center 20%;border-radius:15px;margin-top:15px;margin-bottom:15px;';        
         if (tagline && tagline.nextSibling) {
             tagline.parentNode.insertBefore(personImg, tagline.nextSibling);
         } else if (tagline) {
